fdd7d71b0f7c751fb30519b99964db15071880ac
Romulus Pasca <motoras@gmail.com>
2022-08-13 19:36:05 +0300

    Text codec added

---
+++ examples/echo-repl.rs
@@ -0,0 +1,28 @@
+mod calc;
+
+use dotenv::dotenv;
+use log::info;
+use slean::codec::TextCodec;
+use slean::error::SleanResult;
+use slean::{repl::ReplServer, service::OneReplyService};
+fn echo(req: &String) -> SleanResult<String> {
+    Ok(req.to_string())
+}
+fn main() {
+    dotenv().ok();
+    env_logger::init();
+    let mut handles = Vec::new();
+    for _i in 0..1 {
+        handles.push(std::thread::spawn(run_server));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn run_server() {
+    let service = OneReplyService::<TextCodec, String, String>::new(echo);
+    let mut repl_server = ReplServer::new(service);
+    info!("Starting the server");
+    repl_server.server().unwrap();
+}
--- src/codec.rs
+++ src/codec.rs
@@ -1,16 +1,52 @@
 use std::io::{Read, Write};
 
 use bincode::{deserialize_from, serialize_into};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use rmp_serde::encode::write;
 use rmp_serde::from_read;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
-use crate::error::{SleanError, SleanResult};
+use crate::error::{RemoteError, SleanError, SleanResult};
 
 pub trait Codec<T> {
     fn read(reader: &mut impl Read) -> SleanResult<T>;
     fn write(data: &T, writer: &mut impl Write) -> SleanResult<()>;
+}
+
+pub struct TextCodec {}
+impl Codec<String> for TextCodec {
+    fn read(reader: &mut impl Read) -> SleanResult<String> {
+        let mut res = String::new();
+        reader.read_to_string(&mut res)?;
+        Ok(res)
+    }
+
+    fn write(data: &String, writer: &mut impl Write) -> SleanResult<()> {
+        writer
+            .write_all(&data.as_bytes())
+            .map_err(|err| SleanError::UnexpectedIoFailure(err))
+    }
+}
+
+impl Codec<RemoteError> for TextCodec {
+    fn read(reader: &mut impl Read) -> SleanResult<(i32, String)> {
+        let code = reader
+            .read_i32::<LittleEndian>()
+            .map_err(|err| SleanError::UnexpectedIoFailure(err))?;
+        let mut msg = String::new();
+        reader.read_to_string(&mut msg)?;
+        Ok((code, msg))
+    }
+
+    fn write((code, msg): &RemoteError, writer: &mut impl Write) -> SleanResult<()> {
+        writer
+            .write_i32::<LittleEndian>(*code)
+            .map_err(|err| SleanError::UnexpectedIoFailure(err))?;
+        writer
+            .write_all(&msg.as_bytes())
+            .map_err(|err| SleanError::UnexpectedIoFailure(err))
+    }
 }
 
 pub struct MsgPackCodec {}
